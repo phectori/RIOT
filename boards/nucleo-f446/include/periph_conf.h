7a8a592d9dd229e2f21b6b780c9b3e46c96de7c1
Frits Kuipers <f.p.kuipers@student.utwente.nl>
2017-01-26 20:26:10 +0100

    boards/nucleo-f446: added third SPI definition

--- boards/nucleo-f446/include/periph_conf.h
+++ boards/nucleo-f446/include/periph_conf.h
@@ -200,6 +200,16 @@ static const spi_conf_t spi_config[] = {
         .af       = GPIO_AF5,
         .rccmask  = RCC_APB2ENR_SPI1EN,
         .apbbus   = APB2
+    },
+    {
+        .dev      = SPI3,
+        .mosi_pin = GPIO_PIN(PORT_C, 12),
+        .miso_pin = GPIO_PIN(PORT_C, 11),
+        .sclk_pin = GPIO_PIN(PORT_C, 10),
+        .cs_pin   = GPIO_UNDEF,
+        .af       = GPIO_AF5,
+        .rccmask  = RCC_APB2ENR_SPI1EN,
+        .apbbus   = APB2
     }
 };
 
